d38458dc04458cda847b70fe186f79c382042c58
nguyenvantai5398 <57001938+nguyenvantai5398@users.noreply.github.com>
2019-11-01 21:14:13 +0900

    Code Arduino IDE

---
+++ grove-piezoVibrationSensorAnalog.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    int sensorState = analogRead(35);
+    Serial.println(sensorState);
+    delay(50);
+//    if(sensorState == HIGH)
+//    {
+//     Serial.println("地震です。避難してください。");
+//    }
+//    else
+//    {
+//     Serial.println("安全");
+//    }
+}
